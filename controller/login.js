a56327b31ba2cea3444dd185a10553c3aaca6dd0
mega9789728960 <benmega500@gmail.com>
2025-12-13 13:25:57 +0530

    Fv

--- controller/login.js
+++ controller/login.js
@@ -92,6 +92,7 @@ async function login(req, res) {
             sameSite: "strict",
             maxAge: 24 * 60 * 60 * 1000
         });
+        console.log(user.email,user.username);
 
         return res.status(200).json({
             message: "Login successful",
