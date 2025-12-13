22acfa0da914ca32ba302c31642a63dda273d32e
mega9789728960 <benmega500@gmail.com>
2025-12-13 13:28:51 +0530

    Fv

--- controller/authmiddleware.js
+++ controller/authmiddleware.js
@@ -1,6 +1,7 @@
 import jwt from "jsonwebtoken";
 function authmiddleware(req,res,next){
      const existingToken = req.cookies?.token;
+     console.log("existing token",existingToken);
      if(!existingToken) return res.status(401).json({message:"unauthorized"});
      const decoded=jwt.verify(existingToken,process.env.JWT_SECRET_KEY);
      if(decoded){
